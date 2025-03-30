5aa8b48c620a89c0377233a4691b8739f17c5a80
john-tester-tester <152905643+john-tester-tester@users.noreply.github.com>
2025-03-30 16:01:03 +0100

    update json decode method
    moved the json decode inside a function called
    "return_json_data_from_api_response"
    in case the json decode is missing or the json decode generates an error

--- features/steps/pokemon_api_steps.py
+++ features/steps/pokemon_api_steps.py
@@ -38,63 +38,65 @@ def check_response_value(context, response_value):
 
 @then('response should match values in this expected file "{expected_result_file}" and "{expected_number_of_results}" apis are returned')
 def check_json_response_should_match_values_in_the_expected_file(context, expected_result_file, expected_number_of_results):
-    json_response = context.result.json()
+    json_response = return_json_data_from_api_response(context)
 
-    if json_response:
-        if not (os.path.exists(expected_result_file)):
-            current_path = os.getcwd()
-            assert False, f"Error: File not found, current path {current_path}, expected {expected_result_file}"
-        else:
-            with open(expected_result_file, "r") as import_file:
-                lines = import_file.readlines()
-                number_of_entries = 0
-                for expected_item, (actual_value, actual_path) in zip(lines, json_response.items()):
-                        expected_value, expected_path = str(expected_item).split('\t')
-                        expected_value = str(expected_value).strip().replace('"','')
-                        expected_path = str(expected_path).strip().replace('"','')
-                        assert expected_value == actual_value, f"Error: expected value {expected_value}, actual value {actual_value}"
-                        assert expected_path == actual_path, f"Error: expected path {expected_path}, actual path {actual_path}"
-                        number_of_entries += 1
-
-                assert number_of_entries == int(
-                    expected_number_of_results), (f"Error: incorrect number of apis returned {expected_number_of_results}, "
-                                                  f"actual number returned {number_of_entries}")
+    if not (os.path.exists(expected_result_file)):
+        current_path = os.getcwd()
+        assert False, f"Error: File not found, current path {current_path}, expected {expected_result_file}"
     else:
-        assert False, f"Error: get response is empty"
+        with open(expected_result_file, "r") as import_file:
+            lines = import_file.readlines()
+            number_of_entries = 0
+            for expected_item, (actual_value, actual_path) in zip(lines, json_response.items()):
+                    expected_value, expected_path = str(expected_item).split('\t')
+                    expected_value = str(expected_value).strip().replace('"','')
+                    expected_path = str(expected_path).strip().replace('"','')
+                    assert expected_value == actual_value, f"Error: expected value {expected_value}, actual value {actual_value}"
+                    assert expected_path == actual_path, f"Error: expected path {expected_path}, actual path {actual_path}"
+                    number_of_entries += 1
+
+            assert number_of_entries == int(
+                expected_number_of_results), (f"Error: incorrect number of apis returned {expected_number_of_results}, "
+                                              f"actual number returned {number_of_entries}")
 
 
 @then('the following data is returned')
 def check_json_response_should_match_values_in_the_table(context):
     context.number_of_fields_returned = 0
-    json_response = context.result.json()
+    json_response = return_json_data_from_api_response(context)
     expected_values = {}
     actual_keys =[]
 
-    if json_response:
-        for row in context.table:
-            expected_values.update({row['Field']: row['Value']})
+    for row in context.table:
+        expected_values.update({row['Field']: row['Value']})
 
-        for actual_key, actual_value in json_response.items():
+    for actual_key, actual_value in json_response.items():
 
-            context.number_of_fields_returned +=1
+        context.number_of_fields_returned +=1
 
-            if actual_key in expected_values.keys():
-                assert expected_values[actual_key] == str(actual_value), (f"Error: expected value {expected_values[actual_key]}, "
-                                                                          f"actual value {actual_value}")
-                actual_keys.append(actual_key)
+        if actual_key in expected_values.keys():
+            assert expected_values[actual_key] == str(actual_value), (f"Error: expected value {expected_values[actual_key]}, "
+                                                                      f"actual value {actual_value}")
+            actual_keys.append(actual_key)
 
-        sorted_actual_keys = sorted(actual_keys)
-        sorted_expected_keys = sorted(expected_values.keys())
-        assert sorted_expected_keys == sorted_actual_keys, (f"Error: expected field names {sorted_expected_keys}, "
-                                                                               f"actual field names {sorted_actual_keys}")
-    else:
-        assert False, f"Error: get response is empty"
+    sorted_actual_keys = sorted(actual_keys)
+    sorted_expected_keys = sorted(expected_values.keys())
+    assert sorted_expected_keys == sorted_actual_keys, (f"Error: expected field names {sorted_expected_keys}, "
+                                                                           f"actual field names {sorted_actual_keys}")
 
 
 @then('"{expected_number_of_fields}" fields are present in the data')
 def check_json_response_contains_the_correct_number_of_fields(context, expected_number_of_fields):
     assert expected_number_of_fields == str(context.number_of_fields_returned), (f"Error: expected number of fields {expected_number_of_fields}, "
+
                                                                                  f"actual number of fields {context.number_of_fields_returned}")
+def return_json_data_from_api_response(context):
+    try:
+        return context.result.json()
+    except Exception as e:
+        print(e)
+        assert False, f"Error: unable to extract json data from the API response"
+
 
 if __name__ == '__main__':
     from behave import __main__ as behave_executable
